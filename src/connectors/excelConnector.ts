a3ae49c1379077027ac44da8df186c675a45fd6f
ILIA TISKIN <tiskin2308@gmail.com>
2024-07-06 15:19:15 +0300

    feat: finished core controller for excel authentication

--- src/connectors/excelConnector.ts
+++ src/connectors/excelConnector.ts
@@ -16,4 +16,14 @@ export class ExcelConnector {
 			return null;
 		}
 	}
+
+	public getAuthToken = async (): Promise<string | null> => {
+		try {
+			const response = await axios.get(`${this.url}/get_auth_token`);
+			return response.data;
+		} catch (error) {
+			console.log('Error when getting auth token:', error);
+			return null;
+		}
+	}
 }
--- src/entities/coreServcie.ts
+++ src/entities/coreServcie.ts
@@ -1,13 +1,17 @@
 import { ExcelConnector } from "../connectors/excelConnector";
 
 export class CoreService {
-	private excelConnector: ExcelConnector;
-	
-	constructor() {
-		this.excelConnector = new ExcelConnector();
-	}
+  private excelConnector: ExcelConnector;
 
-	public getAuthUrl = async (): Promise<string | null> => {
+  constructor() {
+    this.excelConnector = new ExcelConnector();
+  }
+
+  public getAuthUrl = async (): Promise<string | null> => {
     return await this.excelConnector.createAuthUrl();
-  };
+  }
+
+  public getAuthToken = async (): Promise<string | null> => {
+	return await this.excelConnector.getAuthToken();
+  }
 }
